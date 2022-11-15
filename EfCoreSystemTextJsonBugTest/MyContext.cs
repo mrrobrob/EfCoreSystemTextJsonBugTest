3a1388a2e495e5361b04c8711fb299911608309e
Robert Page <rob@srcapture.com>
2022-11-15 14:22:06 +0000

    Add project files.

---
+++ EfCoreSystemTextJsonBugTest/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreSystemTextJsonBugTest
+{
+    internal class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
---
+++ EfCoreSystemTextJsonBugTest/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+var thing = System.Text.Json.JsonSerializer.Serialize("testing");
+Console.WriteLine(thing);
