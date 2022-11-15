dde98fe723c66ec653b9f4d659e45122fdd51e33
Robert Page <rob@srcapture.com>
2022-11-15 14:24:14 +0000

    simplify

---
+++ WinFormsApp/Program.cs
@@ -0,0 +1,13 @@
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
